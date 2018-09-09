61ac5162a40d45834e6631033d8b39a3828f7e21
hubertus.putu1 <hubertus.putu1@gmail.com>
2018-09-09 17:38:28 +0700

    fix auto load data

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      users: {},
+      users: [],
       doneFetch: false,
       enabled: true,
-      timeout: 20000,
+      timeout: 10000,
     }
 
     this.renderRow = this.renderRow.bind(this)
@@ -27,13 +27,12 @@ class App extends Component {
 
   setEmptyState = () => {
     this.setState({ 
-      users: {},
+      users: [],
       doneFetch: false,
     })
   }
 
   fetchApi = () => {
-    this.setEmptyState()
     fetch('http://188.166.251.140:4000/data')
     .then(response => {
       return response.json()
@@ -58,48 +57,47 @@ class App extends Component {
   }
   
   renderRow = ({ index, key, style }) => {
-    const { users } = this.state
     return (
       <div key={key} style={style} className='row listBorder'>
         <div className='col-2 image'>
-          <img src={users[index].profilepic} alt='' />
+          <img src={this.state.users[index].profilepic} alt='' />
         </div>
         <div className='col-7 content'>
-          <div>{users[index].full_name}</div>
-          <div className='message'>{users[index].lastMsg}</div>
+          <div>{this.state.users[index].full_name}</div>
+          <div className='message'>{this.state.users[index].lastMsg}</div>
         </div>
         <div className='col detail'>
-          <div className='lastMessage'>{moment().startOf(users[index].lastMsgTime).fromNow()}</div>
-          <div className='float-right'>{this.renderStatus(users[index].isBotOn)}</div>
+          <div className='lastMessage'>{moment().startOf(this.state.users[index].lastMsgTime).fromNow()}</div>
+          <div className='float-right'>{this.renderStatus(this.state.users[index].isBotOn)}</div>
         </div>
       </div>
     );
   }
 
   renderList() {
-    const { doneFetch } = this.state
-    if(doneFetch) {
-      return (
-        <div className='list'>
-          <List
-          width={rowWidth}
-          height={listHeight}
-          rowHeight={rowHeight}
-          rowRenderer={this.renderRow}
-          rowCount={rowCount}
-          overscanRowCount={100} />
-        </div>
-      )
-    }
+    return (
+      <div className='list'>
+        <List
+        width={rowWidth}
+        height={listHeight}
+        rowHeight={rowHeight}
+        rowRenderer={this.renderRow}
+        rowCount={rowCount}
+        overscanRowCount={1}
+        sortBy='lastMsgTime' />
+      </div>
+    )
   }
   
   render() {
-    const { timeout, enabled } = this.state
+    const { timeout, enabled, doneFetch } = this.state
     return (
       <div className='App'>
         <ReactInterval {...{timeout, enabled}}
           callback={() => this.fetchApi()} />
-        {this.renderList()}
+        {doneFetch ? 
+          this.renderList()
+          : null}
       </div>
     );
   }
